2a748a3920960901d2bab271c61d853ecab700be
gozdetekalmaz <gozdetekalmaz@gmail.com>
2020-11-09 11:55:39 +0300

    take real time data with API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.scss';
 import Header from './components/Header/Header';
 import Search from './components/Search/Search';
 import Books from './components/Books/Books';
 
-function App() {
+function App(props) {
+  const [bookList, setBookList] = useState([]);
+
   const books = [
     {
       id: 1,
@@ -53,6 +57,13 @@ function App() {
     },
   ];
 
+  const getBooksOf = async (author) => {
+    const response = await axios.get(
+      `https://www.googleapis.com/books/v1/volumes?q=inauthor:${author}&key=AIzaSyBLy1qIK-9y8FulKeMJZeuQfBK_jqbyxE0`
+    );
+
+    // console.log(response.data.items);
+  };
   return (
     <div className='App'>
       <Header />
--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import axios from 'axios';
 import './Search.scss';
 
 function Search() {
+  let currentSearch = '';
+  const searchAuthor = (e) => {
+    currentSearch = e.target.value;
+    console.log(currentSearch);
+  };
+
   return (
     <section className='search-section'>
       <div className='search-wrapper'>
-        <input type='text' className='book-search' />
+        <input type='text' className='book-search' onChange={searchAuthor} />
+        <button></button>
       </div>
     </section>
   );
